b020fa5e13b2cc266d4c9051e324b356fba74aed
rektrex <psaimanideep98@gmail.com>
2019-01-19 03:36:37 +0530

    add ability to send if the volume is muted

--- volume.sh
+++ volume.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/bash
-awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)
+state=$(amixer sget Master | sed '5q;d' | cut -d " " -f 6,8 | tr -d '[]')
+mute=$(cut -d " " -f 2 <<< $state)
+
+val=$(cut -d " " -f 1 <<< $state)
+
+if [[ "$mute" == "off" ]]
+then
+    val="$val[muted]"
+fi
+
+echo $val
